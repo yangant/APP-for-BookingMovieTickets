d83f00475a0697a35ff5c16efbcb9a67f6432a0d
Sunny <583954049@qq.com>
2017-05-07 10:12:06 +0800

    加入新页与图标
    美工设计好了APP图标并运用上了APP中，完成了注册页的初步设计。

--- MovieBook/app/src/main/java/com/example/tezya/MovieBook/MainActivity.java
+++ MovieBook/app/src/main/java/com/example/tezya/MovieBook/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.tezya.MovieBook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Register(View view) {
+        Intent intent = new Intent(this, RegisterPage.class);
+        startActivity(intent);
     }
 }
